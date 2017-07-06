80cb1c29a0ca0f9caf9e43880a812f9b1d2fc61a
AnbhuSavi <saviprabhu123@gmail.com>
2017-07-06 17:55:08 +0530

    Create OddorEven.java

---
+++ OddorEven.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.*;
+public class OddorEven
+{
+  
+  public static void main(String args[])
+  {
+    
+    int n;
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter a number");
+    n=s.nextInt();
+    if(n%2 == 0)
+    {
+      
+      System.out.println("Even");
+      }
+      else
+      {
+        
+        System.out.println("Odd");
+        }
+        }
+        }
